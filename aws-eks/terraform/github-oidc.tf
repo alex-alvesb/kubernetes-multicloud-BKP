bf6fae59bc44fe3cf06875643a967651c179607e
alex-alvesb <alex_alvesb.jr@hotmail.com>
2026-08-07 15:57:48 -0300

    Corrige thumbprint do OIDC provider do GitHub (usar CA raiz, não o certificado de ponta)

--- aws-eks/terraform/github-oidc.tf
+++ aws-eks/terraform/github-oidc.tf
@@ -5,7 +5,7 @@ data "tls_certificate" "github" {
 resource "aws_iam_openid_connect_provider" "github" {
   url             = "https://token.actions.githubusercontent.com"
   client_id_list  = ["sts.amazonaws.com"]
-  thumbprint_list = [data.tls_certificate.github.certificates[0].sha1_fingerprint]
+  thumbprint_list = [data.tls_certificate.github.certificates[length(data.tls_certificate.github.certificates) - 1].sha1_fingerprint]
 }
 
 resource "aws_iam_role" "github_actions" {
